743851fdfab73beef26372e178945796466e96f5
aragami3070 <smirnov17612@gmail.com>
2024-05-11 19:35:30 +0400

    ITP:Sorts:data_sort:main.cpp: Added date structure and function of converting a line to a date

---
+++ ITP/Sorts/data_sort/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+struct date_of_birth {//структура для данных даты рождения 
+	int day, month, year;
+};
+
+//функция превода строки в дату
+date_of_birth str_to_date(string str) {
+	date_of_birth date;
+	date.day = (str[0] - '0') * 10 + str[1] - '0';
+	date.month = (str[3] - '0') * 10 + str[4] - '0';
+	date.year = (str[6] - '0') * 1000 + (str[7] - '0') * 100 +
+		(str[8] - '0') * 10 + str[9] - '0';
+	return date;
+}
