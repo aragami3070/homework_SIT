beaae9a42f116b6d117c62f492bada77294afd6f
aragami3070 <smirnov17612@gmail.com>
2024-05-16 00:01:39 +0400

    ITP:Dynamic_struct:double_linked_list:task_1.cpp: Added function that adds last odd after each min

--- ITP/Dynamic_struct/double_linked_list/task_1.cpp
+++ ITP/Dynamic_struct/double_linked_list/task_1.cpp
@@ -78,3 +78,70 @@ void del_list(list*& head, list*& tail) {
 		}
 	}
 }
+
+
+
+/*
+Создать список, содержащий целые числа.
+После всех чисел, равных минимальному числу, вставить последний нечетный элемент.
+Например, было  2 8 2 1 6 8 8 1 2 2 8 2 1 3
+Стало  2 8 2 1 3 6 8 8 1 3 2 2 8 2 1 3
+*/
+
+void change_1(list*& head, list*& tail, int mmin, int last_odd) {
+	list* temp = head;//создаем доп указатель на голову 
+	while (temp != tail->next) {
+		//если элемент на который указывает temp = mmin
+		if (temp->inf == mmin) {
+			//то добавляем last_odd (значение последнего нечетного элемента)
+			insert_after(head, tail, temp, last_odd);
+			temp = temp->next;//сдвигаем указатель
+		}
+		temp = temp->next;//сдвигаем указатель
+	}
+}
+
+
+void task_1() {
+	setlocale(LC_ALL, "RUS");
+	list* mas_head = NULL;//создали два указателя для списка
+	list* mas_tail = NULL;
+	int len;
+	cout << "Введите кол-во элементов списка:" << endl;
+	cin >> len;
+	int mmin, last_odd;
+	cout << "Введите элементы списка:" << endl;
+	//заполняем массив
+	for (int i = 0; i < len; i++) {
+		int temp;
+		cin >> temp;//ввод элемента
+		push_back(mas_head, mas_tail, temp);//добавляем элемент в массив
+		//находим минимальный элемент
+		if (i == 0) {
+			mmin = temp;//ставим первый элемент как минимальный
+		}
+		if (mmin > temp) {
+			mmin = temp;
+		}
+		//находим последний нечетный элемент
+		if (temp % 2 != 0) {
+			last_odd = temp;
+		}
+	}
+	//после каждого mmin ставим last_odd
+	change_1(mas_head, mas_tail, mmin, last_odd);
+	//вывод 
+	cout << "Ответ:" << endl;
+	print(mas_head, mas_tail);
+	//удаление всего массива
+	del_list(mas_head, mas_tail);
+}
+/*
+test
+14
+2 8 2 1 6 8 8 1 2 2 8 2 1 3
+*/
+
+int main(){
+    task_1();
+}
