6c8111a23dd44cf70fbfab3b35068a9d941f79cd
aragami3070 <smirnov17612@gmail.com>
2024-05-18 00:01:37 +0400

    ITP:Tree:task_tree.cpp: Added insert function

--- ITP/Tree/task_tree.cpp
+++ ITP/Tree/task_tree.cpp
@@ -21,3 +21,46 @@ tree* node(int value) {
 }
 
 
+
+void insert(tree*& tr, int value) {
+	tree* new_node = node(value);
+	if (!tr) {//если дерево пустое
+		tr = new_node;//то новый элемент корень
+	}
+	else {
+		//создаем доп указатель на дерево
+		tree* temp = tr;
+		while (temp) {
+			//если новый элемент больше чем элемент на который указывает temp
+			if (new_node->inf > temp->inf){
+				//если у temp есть правый ребенок 
+				if (temp->right) {
+					//то смещаем указатель temp на этого ребенка
+					temp = temp->right;
+				}
+				//иначе новый элемент - это правый ребенок temp
+				else {
+					new_node->parent = temp;
+					temp->right = new_node;
+					break;
+				}
+			}
+			//иначе если новый элемент меньше чем элемент на который указывает temp
+			else if (new_node->inf < temp->inf) {
+				//если у temp есть левый ребенок
+				if (temp->left) {
+					//то смещаем указатель temp на этого ребенка
+					temp = temp->left;
+				}
+				//иначе новый элемент - это левый ребенок temp
+				else {
+					new_node->parent = temp;
+					temp->left = new_node;
+					break;
+				}
+			}
+
+		}
+	}
+}
+
