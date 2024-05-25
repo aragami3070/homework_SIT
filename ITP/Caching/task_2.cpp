501ba05d192406124322fa0bbb4bd279dc5c5efa
aragami3070 <smirnov17612@gmail.com>
2024-05-26 02:43:07 +0400

    ITP:Caching:task_2.cpp: Added date_of_birth struct with str_to_date function

---
+++ ITP/Caching/task_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+
+struct date_of_birth {//структура для данных даты рождения 
+	int day = 0, month = 0, year = 0;
+};
+
+//функция превода строки в дату
+date_of_birth str_to_date(string str) {
+	date_of_birth date;
+	date.day = (str[0] - '0') * 10 + str[1] - '0';
+	date.month = (str[3] - '0') * 10 + str[4] - '0';
+	date.year = (str[6] - '0') * 1000 + (str[7] - '0') * 100 +
+		(str[8] - '0') * 10 + str[9] - '0';
+	return date;
+}
